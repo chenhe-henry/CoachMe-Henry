72cb370db58f12d8b18a81eabb2e3c09b43e42d6
chenhe-henry <chenhe.elec@gmail.com>
2021-11-28 11:47:57 +1100

    fetch data from api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,31 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "Home",
   components: {
     CoachList: () => import("@/components/CoachList"),
+  },
+  data() {
+    return {
+      coachData: [],
+    };
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    // init fetch data from api
+    init() {
+      axios
+        .get(
+          "https://raw.githubusercontent.com/suyogshiftcare/jsontest/main/available.json"
+        )
+        .then((res) => {
+          this.coachData = res.data;
+        });
+    },
   },
 };
 </script>
