063e274c6f2e9330d733ff1e2ba136d60ad3a0f4
chenhe-henry <chenhe.elec@gmail.com>
2021-11-28 12:08:37 +1100

    format coach list

--- src/components/CoachList.vue
+++ src/components/CoachList.vue
@@ -1,9 +1,84 @@
 <template>
-  <div>Coach List</div>
+  <div class="coach-list">
+    <div
+      v-for="coach in coachSimpleData"
+      :key="coach.name"
+      @click.stop="getCoach(coach)"
+      class="coach"
+    >
+      {{ coach.name }}
+      {{ coach.timezone }}
+      <div v-for="day in coach.availablity" :key="day">
+        {{ convertDay(day) }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "CoachList",
+  props: {
+    coachSimpleData: { type: Array, default: () => [] },
+  },
+  data() {
+    return {};
+  },
+
+  methods: {
+    // emit selected coach's name/timezone info to parent
+    getCoach(e) {
+      this.$emit("click", { name: e.name, timezone: e.timezone });
+    },
+    convertDay(day) {
+      if (day === "Monday") {
+        return "Mon";
+      }
+      if (day === "Tuesday") {
+        return "Tue";
+      }
+      if (day === "Wednesday") {
+        return "Wed";
+      }
+      if (day === "Thursday") {
+        return "Thur";
+      }
+      if (day === "Friday") {
+        return "Fri";
+      }
+      if (day === "Saturday") {
+        return "Sat";
+      }
+      if (day === "Sunday") {
+        return "Sun";
+      }
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped lang="scss">
+.coach {
+  cursor: pointer;
+  padding: 2rem 1rem;
+  border: 2px solid #c4c4c4;
+  border-radius: 10px;
+  width: 10vw;
+  margin: 0 auto;
+  transition: all 0.2s;
+  &:hover {
+    background: #d4d4d4;
+    transform: scale(1.1);
+  }
+  &-list {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 1rem;
+    width: 50vw;
+    margin: 0 auto;
+    background: #f4f4f4;
+    padding: 4rem 0;
+    border-radius: 10px;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <CoachList />
+    <CoachList :coachSimpleData="coachSimpleData" @click="selectCoach" />
   </div>
 </template>
 
@@ -14,12 +14,38 @@ export default {
   data() {
     return {
       coachData: [],
+      coachSimpleData: [],
     };
   },
   mounted() {
     this.init();
   },
   methods: {
+    selectCoach(e) {
+      this.selectedCoach = e;
+      console.log("e :>> ", e);
+    },
+    // restructure coach data based on filtered data
+    getCoachInfo(obj) {
+      let availablity = [];
+      obj.forEach((e) => {
+        availablity.push(e["day_of_week"]);
+      });
+      return {
+        name: obj[0].name,
+        timezone: obj[0].timezone,
+        availablity: [...new Set(availablity)],
+      };
+    },
+    formatData() {
+      const coachArr = this.coachData.map((e) => e.name);
+      let coaches = [...new Set(coachArr)];
+      coaches.forEach((e) =>
+        this.coachSimpleData.push(
+          this.getCoachInfo(this.coachData.filter((el) => el.name === e))
+        )
+      );
+    },
     // init fetch data from api
     init() {
       axios
@@ -28,6 +54,9 @@ export default {
         )
         .then((res) => {
           this.coachData = res.data;
+        })
+        .then(() => {
+          this.formatData();
         });
     },
   },
